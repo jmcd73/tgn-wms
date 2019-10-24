f01cc403d4d3460b5339eb2ee89d29071e92b2af
James McDonald <james@toggen.com.au>
2019-10-24 21:12:45 +1100

    add react pages

---
+++ app/webroot/shipment-app/precache-manifest.782a6ba721062330d27a2ecaaaaabbe6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b8a3bc45d5c1e507139bfc0c9b93298",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35fabe6758f56189951e",
+    "url": "/static/css/2.e31a430a.chunk.css"
+  },
+  {
+    "revision": "4bb4862ea0aae4dd9e8b",
+    "url": "/static/css/main.4bea5fb0.chunk.css"
+  },
+  {
+    "revision": "35fabe6758f56189951e",
+    "url": "/static/js/2.5a03812a.chunk.js"
+  },
+  {
+    "revision": "4bb4862ea0aae4dd9e8b",
+    "url": "/static/js/main.f4237c9b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
