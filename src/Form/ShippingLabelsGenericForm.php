29da07bb83771a121f2b556273aa1fc51a5aa883
James McDonald <james@toggen.com.au>
2020-04-22 11:05:08 +1000

    Add max copies to ShippingLabelsGenericForm

--- src/Form/ShippingLabelsGenericForm.php
+++ src/Form/ShippingLabelsGenericForm.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace App\Form;
 
+use Cake\Core\Configure;
+use Cake\Event\EventManager;
 use Cake\Form\Form;
 use Cake\Form\Schema;
 use Cake\Validation\Validator;
@@ -12,32 +14,41 @@ use Cake\Validation\Validator;
  */
 class ShippingLabelsGenericForm extends Form
 {
+    protected $maxCopies = 1000;
+
+    public function __construct(?EventManager $eventManager = null)
+    {
+        parent::__construct($eventManager);
+
+        $this->maxCopies = Configure::read('MAX_COPIES');
+    }
+
     /**
      * Builds the schema for the modelless form
      *
-     * @param \Cake\Form\Schema $schema From schema
+     * @param  \Cake\Form\Schema $schema From schema
      * @return \Cake\Form\Schema
      */
     protected function _buildSchema(Schema $schema): Schema
     {
-        return $schema;
+        return $schema->addField('copies', 'integer');
     }
 
     /**
      * Form validation builder
      *
-     * @param \Cake\Validation\Validator $validator to use against the form
+     * @param  \Cake\Validation\Validator $validator to use against the form
      * @return \Cake\Validation\Validator
      */
     public function validationDefault(Validator $validator): Validator
     {
-        return $validator;
+        return $validator->lessThanOrEqual('copies', $this->maxCopies, 'Copies must be no more than ' . $this->maxCopies);
     }
 
     /**
      * Defines what to execute once the Form is processed
      *
-     * @param array $data Form data.
+     * @param  array $data Form data.
      * @return bool
      */
     protected function _execute(array $data): bool
