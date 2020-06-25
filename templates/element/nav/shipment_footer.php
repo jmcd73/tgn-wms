8b26ef3ca3ef4fe9c1bfc72b36028ced6b7d772b
James McDonald <james@toggen.com.au>
2020-06-26 08:23:54 +1000

    Remove uneeded divs

--- templates/element/nav/shipment_footer.php
+++ templates/element/nav/shipment_footer.php
@@ -1,9 +1,7 @@
 <div class="navbar fixed-bottom navbar-light bg-light">
-    <div class="container">
         <div class="row w-100 text-center">
             <div class="col">
                 <button type="submit" class="btn btn-primary">Submit</button>
             </div>
         </div>
-    </div>
 </div>
